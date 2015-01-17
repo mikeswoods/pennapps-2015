440cc96f0a9bf602b184483b9cfa5431f4c6829c
Stuart Wagner <stuwags@gmail.com>
2015-01-17 13:35:54 -0500

    Latest web app

--- webapp/wa.py
+++ webapp/wa.py
@@ -9,7 +9,7 @@ def do_upload():
     if ext not in ('.png', '.jpg', '.jpeg'):
         return "File extension not allowed."
 
-    save_path = "/tmp/{email}".format(email=email)
+    save_path = "/Users/stuwagsmac/pennapps/pennapps-2015/webapp/static"
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
@@ -17,12 +17,26 @@ def do_upload():
     upload.save(file_path)
     return "File successfully saved to '{0}'.".format(save_path)
 
+@route('/static/<filename>')
+def server_static(filename):
+  return static_file(filename,root='/Users/stuwagsmac/pennapps/pennapps-2015/webapp/static')
 
-@route('/index')
+@route('/static/stuwags@gmail.com/<filename>')
+def server_static(filename):
+  return static_file(filename,root='/Users/stuwagsmac/pennapps/pennapps-2015/webapp/static')
+
+@route('/')
 def index():
 	doc = """
 	<!DOCTYPE html> <html>
 	<head>
+  <p>Enter your email address</p>
+  <script>
+  var email = document.createElement("INPUT");
+  x.setAttribute("type", "text");
+  x.setAttribute("value", "Enter your email address.");
+  document.body.appendChild(x);
+  </script>
 	<link rel="stylesheet" href="//assets.ziggeo.com/css/ziggeo-betajs-player.min.css" />
 	<script src="//assets.ziggeo.com/js/ziggeo-jquery-json2-betajs-player.min.js"></script>
 	<script>ZiggeoApi.token = "ded1df4caa4dfe85e18a82d451df14ad";</script>
@@ -33,9 +47,9 @@ def index():
     </body>
    	</html>
 
-   	ZiggeoApi.Events.on("submitted", function (data) {
+  <script>ZiggeoApi.Events.on("submitted", function (data) {
 	alert("Submitted a new video with token '" + data.video.token + "'!");
-});
+  });</script>
 
 	<form action="/upload" method="post" enctype="multipart/form-data">
   	Email:      <input type="text" name="email" />
@@ -44,7 +58,6 @@ def index():
 	</form>
 		"""
 	return doc
-
 
 
 
