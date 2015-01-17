8b8fe861388f5824403b89a6cc7b1d059a2c80ca
Mike Woods <mikeswoods@gmail.com>
2015-01-17 16:23:31 -0500

    Updated

--- engine/__main__.py
+++ engine/__main__.py
@@ -10,6 +10,6 @@ output_image = join(getcwd(), 'mosaic_' + basename(input_image))
 
 print "<INPUT> {}\n<OUTPUT> {}".format(input_image, output_image)
 
-engine.mosaic.create(input_image, output_image, k=4, n=12, start_window=120, end_window=8)
+engine.mosaic.create(input_image, output_image, k=4, n=15, start_window=120, end_window=12)
 
 ################################################################################
