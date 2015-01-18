6d444fb83489f232abd01b1f3413fd5b2f1dd0fa
Stuart Wagner <stuwags@gmail.com>
2015-01-17 20:11:24 -0500

    Got gif maker emojifying each frame

--- make_gif.py
+++ make_gif.py
@@ -1,4 +1,6 @@
 import cv2, sys, tempfile
+import engine.mosaic
+from os.path import join, basename
 
 try:
     vidFile = cv2.VideoCapture(sys.argv[1])
@@ -16,17 +18,20 @@ print "FPS value: %s" %fps
 frame_iter = nFrames/10 #30 frames returned
 count = 0
 
-frames = []
+emojified_frame_locations = []
 ret = 1 #initialize
 while ret:  # note that we don't have to use frame number here, we could read from a live written file.
     if count % frame_iter == 0:
-    	ret, frame = vidFile.read() # read first frame, and the return code of the function.
-    	temp = tempfile.NamedTemporaryFile()
-    	frames.append(temp.name)
-    	print frame
-    	print count, temp.name
+        ret, frame = vidFile.read() # read first frame, and the return code of the function.
+        temp = tempfile.NamedTemporaryFile(delete=False)
+        print temp.name
+        temp.name = temp.name + '.jpg'
+        engine.mosaic.create(frame, temp.name)
+        emojified_frame_locations.append(temp.name)
+        print count, temp.name
+        print emojified_frame_locations
     else:
-    	ret = vidFile.grab()
+        ret = vidFile.grab()
     #cv2.imshow("frameWindow", frame)
     #cv2.waitKey(int(1/fps*1000)) # time to wait between frames, in mSec
     count += 1
